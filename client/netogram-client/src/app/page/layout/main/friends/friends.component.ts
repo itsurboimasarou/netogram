a018993bfcd6045f97f18cfccacb53a363f0146b
thanhhuy5902 <tranthanhhuy42@gmail.com>
2024-08-11 15:13:37 +0700

    fix navigate friend

--- client/netogram-client/src/app/page/layout/main/friends/friends.component.ts
+++ client/netogram-client/src/app/page/layout/main/friends/friends.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MaterialModule} from "../../../../shared/material.module";
+import {ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, RouterOutlet, RouterLink, RouterLinkActive, NgForOf],
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.scss'
 })
-export class FriendsComponent {
+export class FriendsComponent implements OnInit {
+  navLinks: any[];
+  activeLinkIndex = -1;
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.navLinks = [
+      {
+        label: 'Friends',
+        link: './friend list',
+        index: 0
+      }, {
+        label: 'Friend requests',
+        link: './friend request',
+        index: 1
+      },
+    ];
 
+
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
 }
